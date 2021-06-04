760d050b7f027c855e8e0d5234d81ff207b248d3
joankalanzi <jaon.kalanzi1@gmail.comgit config --global user.name joan>
2021-06-04 14:27:51 +0100

    final

--- scripts/app.js
+++ scripts/app.js
@@ -57,7 +57,7 @@ function init() {
    function createGrid(startingFrogPosition){
        for (let i = 0; i < cellCount; i++){
        const cell = document.createElement('div')
-       cell.innerText = i
+      //  cell.innerText = i
        grid.appendChild(cell)
        cells.push(cell)
        } 
